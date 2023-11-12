298a5a29d8c12d6c46bb353e865240dde6186f27
Yashwanth Javvaji <yashwanth.javvaji@Yashwanths-MacBook-Pro.local>
2023-11-11 19:18:23 -0600

    Updated experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -3,15 +3,25 @@ import Position from "~/components/Position";
 
 const experiences = [
     {
+        "Title": "Software Engineer Intern",
+        "Company name": "Tesla",
+        "Start date": "August 2023",
+        "End date": "Present",
+        "Description": [
+            "Instrumental the development and ongoing maintenance of production-critical code within the Material Flow System (MFS) at Giga Texas, ensuring the uninterrupted production of Model Y and Cybertrucks.",
+            "Created an Order Troubleshooting System within MFS, efficiently tracking various production flows, validating demand configurations, and ensuring proper station signal transmission. Provides a detailed checklist of critical steps with success/failure status and potential issue reasons for swift support team resolution, bolstering overall production efficiency.",
+            "Improved the Gantry Unload functionality, enhancing user experience by implementing features such as real-time display of active sessions, their potential conflicts, and providing clear progress indicators for containers scanned along with their scan statuses resulting in smoother operations and minimized material handling errors."
+        ]
+    },
+    {
         "Title": "Software Dev Engineer Intern",
         "Company name": "Amazon Web Services (AWS)",
         "Start date": "May 2023",
-        "End date": "Present",
+        "End date": "August 2023",
         "Description": [
-            "Implemented an advanced validation system, transforming artifacts validation using intricate rules configurations. Extended the system's capabilities to dynamically handle diverse input/output fields and adapt to evolving configuration criteria.",
-            "Increased test code coverage to exceed 90% across core artifacts repository service packages. Created integration tests, ensuring seamless operation and robustness of services.",
-            "Utilized Evidently to conduct informative A/B testing, delivering data-driven insights into configuration impact. Enabled dynamic configuration toggling for flexible activation and deactivation of artifact validation.",
-            "Resolved 3 critical policy engine issues within pipelines and proactively engaged in high-priority initiatives, contributing to the timely readiness of the service for its beta release."
+            "Designed and implemented a Validation Lambda function enabling secure file-based communication between customers and partners within the AWS Supply Chain. This system validates shared artifacts against predefined rules, enhancing data integrity, compliance, and efficiency in various supply chain processes such as invoicing.",
+            "Utilized Amazon's Evidently tool for A/B testing, providing data-driven insights, including request response time. Implemented a feature toggle for dynamic enablement or disablement based on project requirements.",
+            "Resolved 2 critical policy engine issues in artifacts service code pipeline: Added bake time approval for production stages, ensuring careful monitoring for slow-burning impacts post-deployment, preventing adverse effects across regions; Restructured the pipeline to feature separate stages for each AWS region, ensuring regional isolation, geographical redundancy, and efficient management of region-specific configurations, enhancing deployment resilience and accuracy."
         ]
     },
     {
@@ -20,10 +30,8 @@ const experiences = [
         "Start date": "January 2023",
         "End date": "April 2023",
         "Description": [
-            "Successfully finished the Microsoft Ambassador Program, an esteemed initiative that enhances knowledge and skills related to Microsoft 365.",
-            "Developed advanced abilities in utilizing Microsoft 365 tools and technologies through interactive workshops and training sessions.",
-            "Delivered a final presentation on Microsoft Integration to diverse audiences, effectively communicating complex concepts.",
-            "Engaged in team-based activities and workshops, fostering effective communication, problem-solving, and collaboration skills."
+            "Successfully completed the Microsoft Ambassador Program, honing expertise in Microsoft 365 tools and technologies through immersive workshops and training sessions.",
+            "Demonstrated proficiency by delivering a comprehensive presentation on Microsoft Integration to diverse audiences. Engaged in team-based activities and workshops, fostering effective communication, problem-solving, and collaboration skills."
         ]
     },
     {
@@ -32,11 +40,8 @@ const experiences = [
         "Start date": "February 2023",
         "End date": "March 2023",
         "Description": [
-            "Contributed to a funded project focused on detecting and defending against software vulnerabilities, strengthening cybersecurity practices.",
-            "Generated high-quality vulnerability data for the most common types of vulnerabilities, improving software vulnerability detection and repair tools.",
-            "Collaborated in a team to label 300 code samples, injecting 66 realistic vulnerabilities while preserving core functionality.",
-            "Utilized C programming expertise and knowledge of common code structures to identify and introduce vulnerabilities accurately.",
-            "Communicated progress and worked closely with team members, ensuring a collaborative and efficient workflow."
+            "Collaborated with a research team on an ongoing project aimed at developing a Neural Network model for detecting potential errors in C programs, including memory leaks, overflows, user after free, double free, etc.",
+            "Played a pivotal role by generating a dataset of over 500 vulnerable C programs and providing accurate labeling, addressing the data scarcity issue, and facilitating the training of the error identification model."
         ]
     },
     {
@@ -45,11 +50,8 @@ const experiences = [
         "Start date": "January 2022",
         "End date": "June 2022",
         "Description": [
-            "Developed a JavaScript application that improved automation efficiency by suggesting optimal completion times, resulting in a significant 63% reduction in time taken.",
-            "Created 100% automated dashboards for Service Usage and Health Analytics (SUHA) across various services within our team.",
-            "Wrote clean and well-tested code, along with technical documentation, for the JavaScript application and SUHA dashboard projects.",
-            "Identified and implemented key features to meet customer requirements through consistent software development.",
-            "Utilized Scrum and Agile methodologies to deliver well-structured and timely solutions."
+            "Implemented an application that utilizes historical instance data and time series modeling technique to optimize the automation processes, such as backups and restores, for ServiceNow instances, leading to substantial reductions in execution times to about 63% and improved operational efficiency.",
+            "Developed 100% automated Service Usage and Health Analytics (SUHA) dashboards for team-owned services, enabling real-time monitoring of crucial parameters such as service load and request volume. Utilized Impala SQL to extract and transform data from our extensive data storage infrastructure."
         ]
     },
     {
@@ -58,10 +60,8 @@ const experiences = [
         "Start date": "April 2021",
         "End date": "July 2022",
         "Description": [
-            "Maintained accurate records of student placements and internship opportunities and provided regular reports to the department head.",
-            "Organized and coordinated on-campus recruitment events, such as job fairs and company presentations, to connect students with potential employers.",
-            "Provided support and guidance to students throughout the placement process, including resume and cover letter writing, interview preparation, and job search strategies.",
-            "Improved the number of placements in the computer science department by more than 100 as compared to the previous year."
+            "Efficiently managed student placement records and internship opportunities, delivering regular reports to the department head. Orchestrated on-campus recruitment events, including job fairs and company presentations, facilitating valuable connections between students and potential employers.",
+            "Offered comprehensive support to students during the placement process, encompassing resume and cover letter crafting, interview readiness, and effective job search strategies. Resulted in a remarkable increase of over 100 placements within the computer science department compared to the previous year."
         ]
     },
     {
@@ -70,10 +70,8 @@ const experiences = [
         "Start date": "April 2021",
         "End date": "July 2021",
         "Description": [
-            "Implemented the front-end component of the web application for the merchandise upload facility utilizing the NextJS framework, achieving about 1.5 times increase in productivity.",
-            "Performed thorough manual testing on the web application, checking for various test cases to ensure high-quality software and identify any issues or bugs.",
-            "To make the system 100% accurate, wrote postman tests to verify that Okta authentication is operating as intended, returning user information legitimately, service integrations are reliable, and any modifications haven’t disrupted current functionality.",
-            "Collaborated with a cross-functional team of developers, product managers, and QA engineers to design and implement new features, identify and resolve bugs, and improve the overall user experience."
+            "Created a user-friendly front-end component that streamlined vendor item requests and approval workflows, including L1 and L2 approvals. Utilized ag-grid to provide robust data management capabilities, enhancing data accessibility and facilitating efficient decision-making for requesters and approvers.",
+            "Authored a suite of API tests to guarantee the system's precision, covering Okta authentication, retrieval of user information, service integrations. These tests ensured a reliable and accurate system performance."
         ]
     },
     {
@@ -82,10 +80,8 @@ const experiences = [
         "Start date": "May 2020",
         "End date": "June 2020",
         "Description": [
-            "Developed a streamlined application for diagnostic test ordering, reducing paperwork by 75% through doctor prescription uploads and optimizing the user experience.",
-            "Designed of the application's database structure and created wireframes using Adobe, providing a solid foundation for implementation.",
-            "Conducted comprehensive testing to ensure cross-browser functionality and device compatibility, addressing any issues and optimizing code for optimal performance across various devices and browsers.",
-            "Collaborated closely with project mentor to integrate new design features and ensure adherence to industry best practices, resulting in a secure web application that met high standards of web security."
+            "Developed a user-friendly diagnostic test ordering application that reduced paperwork by 75% by allowing doctors to upload prescriptions digitally, streamlining the process and enhancing eco-friendliness.",
+            "Independently led database design, wireframing, cross-browser testing, and security enhancements, actively incorporating valuable feedback from a mentor. This ensured the development of a responsive and secure web application meeting industry standards."
         ]
     }
 ];
