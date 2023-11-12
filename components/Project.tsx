ca242b57592ce12741233181f63babdabea1b388
Yashwanth Javvaji <yashwanth.javvaji@Yashwanths-MacBook-Pro.local>
2023-11-11 19:18:23 -0600

    Updated experience

--- components/Project.tsx
+++ components/Project.tsx
@@ -21,11 +21,11 @@ const capitalize = (strings: string[]) => {
 const Project = ({ project }: Props) => {
     return (
         <div className="rounded overflow-hidden shadow-2xl hover:scale-105 hover:shadow-3xl transition duration-300 ease-in-out">
-            <Link href={project.html_url} target="_blank" rel="noreferrer">
+            <Link href={project.html_url} target="_blank" rel="noreferrer" className="cursor-pointer">
                 <div className="px-6 py-4">
                     <div className="flex flex-row justify-between items-center">
                         <p className="font-bold text-xl mb-1 text-gray-800 dark:text-gray-100">{capitalize(project.name.split("-")).join(" ")}</p>
-                        <FaGithub className="block w-8 h-8 text-black dark:text-white ml-3 my-2 cursor-pointer transition duration-100" />
+                        <FaGithub className="block w-8 h-8 text-black dark:text-white ml-3 my-2" />
                     </div>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">- {moment(project.created_at).format("MMMM YYYY")}</p>
                     <p className="text-gray-900 dark:text-gray-50 text-base">{project.description}</p>
