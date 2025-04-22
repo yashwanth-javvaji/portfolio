9939fc059421ee563e04c6a1aa9199cc881bd45f
Yashwanth Javvaji <yashwanthjavvaji208@gmail.com>
2025-04-22 01:19:53 -0500

    Updated professional details (Apr 22, 2025)

--- components/About.tsx
+++ components/About.tsx
@@ -7,16 +7,16 @@ const About = () => {
             </h3>
             <div className="space-y-3 text-black dark:text-white text-lg text-justify">
                 <p className="[text-align-last:center]">
-                    I'm Yashwanth Javvaji, a results-driven Software Engineer with over 3 years of experience building secure, scalable, and high-performance systems across domains like cloud infrastructure, supply chain automation, and real-time data platforms. Currently, I’m part of the Amazon Web Services (AWS) team, where I work on cutting-edge systems that power inventory management and workforce solutions at scale.
+                    I&apos;m Yashwanth Javvaji, a results-driven Software Engineer with over 3 years of experience building secure, scalable, and high-performance systems across domains like cloud infrastructure, supply chain automation, and real-time data platforms. Currently, I&apos;m part of the Amazon Web Services (AWS) team, where I work on cutting-edge systems that power inventory management and workforce solutions at scale.
                 </p>
                 <p className="[text-align-last:center]">
-                    Previously, I’ve had the opportunity to intern at top-tier companies like Tesla, ServiceNow, and Advance Auto Parts, where I led critical projects involving real-time tracking systems, GenAI integrations, and automation tooling. My tech stack spans GO, Kotlin, TypeScript, and frameworks like React, Spring, and Node.js. I’m equally at home deploying infrastructure with AWS CDK and Kubernetes as I am designing APIs or writing clean, maintainable frontend code.
+                    Previously, I&apos;ve had the opportunity to intern at top-tier companies like Tesla, ServiceNow, and Advance Auto Parts, where I led critical projects involving real-time tracking systems, GenAI integrations, and automation tooling. My tech stack spans GO, Kotlin, TypeScript, and frameworks like React, Spring, and Node.js. I&apos;m equally at home deploying infrastructure with AWS CDK and Kubernetes as I am designing APIs or writing clean, maintainable frontend code.
                 </p>
                 <p className="[text-align-last:center]">
-                    Beyond technical work, I’m passionate about building systems that are not just functional, but also elegant, efficient, and secure. I thrive in environments where innovation meets impact, and I'm always exploring new ways to bridge complex engineering with user-centric design.
+                    Beyond technical work, I&apos;m passionate about building systems that are not just functional, but also elegant, efficient, and secure. I thrive in environments where innovation meets impact, and I&apos;m always exploring new ways to bridge complex engineering with user-centric design.
                 </p>
                 <p className="[text-align-last:center]">
-                    Whether it's optimizing system performance, improving developer workflows, or diving into emerging tech like GenAI, I’m driven by a constant desire to learn, build, and solve meaningful problems.
+                    Whether it&apos;s optimizing system performance, improving developer workflows, or diving into emerging tech like GenAI, I&apos;m driven by a constant desire to learn, build, and solve meaningful problems.
                 </p>
             </div>
         </>
